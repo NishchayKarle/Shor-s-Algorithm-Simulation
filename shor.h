86ae694d52ac16c9f8a2b953e4d685031c66c50a
Tina <tina@Tinas-MacBook-Air.local>
2023-05-15 15:05:55 -0500

    shor.cpp

--- shor.h
+++ shor.h
@@ -9,7 +9,7 @@ void QFT(Register *reg, unsigned int start = 0, unsigned int end = 0);
 
 void IQFT(Register *reg, unsigned int start = 0, unsigned int end = 0);
 
-unsigned int find_Shor_period(unsigned int a, unsigned int N, unsigned int depth_limit = 200);
+unsigned int find_Shor_period(unsigned int a, unsigned int N, unsigned int depth_limit = 20);
 
 unsigned int Shor(unsigned int N, unsigned int depth_limit = 200);
 
